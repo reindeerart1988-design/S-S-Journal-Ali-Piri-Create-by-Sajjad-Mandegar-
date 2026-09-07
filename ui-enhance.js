159d266aa6c76fdbb5b055796f4d5abfe36c064e
Sajjad Mandegar <reindeerart1988@gmail.com>
2026-09-08 00:47:09 +0330

    Update ui-enhance.js

--- ui-enhance.js
+++ ui-enhance.js
@@ -45,8 +45,8 @@
       var r = active.getBoundingClientRect();
       pill.style.left = (r.left - navRect.left) + 'px';
       pill.style.width = r.width + 'px';
-      pill.style.top = (r.top - navRect.top) + 'px';
-      pill.style.height = r.height + 'px';
+      pill.style.top = '0';
+      pill.style.height = '100%';
       pill.classList.add('on');
     }
 
