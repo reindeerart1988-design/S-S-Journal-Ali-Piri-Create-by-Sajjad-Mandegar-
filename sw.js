764e5155ca9a952a86186dd8f1597907a690d47b
Sajjad Mandegar <reindeerart1988@gmail.com>
2026-09-08 08:49:22 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='ss-journal-v10';
-const APP_SHELL=['./','./index.html','./styles.css?v=10','./app.js?v=10','./ui-enhance.js?v=10','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ss-journal-')&&k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- const r=e.request;if(r.method!=='GET'||new URL(r.url).origin!==self.location.origin)return;
- e.respondWith(fetch(r).then(res=>{if(res.ok){const copy=res.clone();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.put(r,copy)));}return res;}).catch(async()=>await caches.match(r)||Response.error()));
-});
