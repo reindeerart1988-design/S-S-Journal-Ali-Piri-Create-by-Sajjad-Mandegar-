44aee3297ac0195dcb4e1069ab1c86bf0949f360
Sajjad Mandegar <reindeerart1988@gmail.com>
2026-09-12 10:36:57 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='ss-journal-v20';
-const APP_SHELL=['./','./index.html','./styles.css?v=20','./app.js?v=20','./ui-enhance.js?v=20','./ux.js?v=20','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='ss-journal-v21';
+const APP_SHELL=['./','./index.html','./styles.css?v=21','./app.js?v=21','./ui-enhance.js?v=21','./ux.js?v=21','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ss-journal-')&&k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
